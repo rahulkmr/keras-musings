e6c171f4c1c3be6ebfc8b9dec328cde786c2a3ac
Rahul Kumar <rahul@thoughtnirvana.com>
2019-01-31 12:51:22 +0530

    cats and dogs classifier with pretrained convnet

---
+++ cats_dogs_pretrained.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+from keras.applications import VGG16
+from keras import models, layers, optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+base_dir = '/Users/dprasad011/Downloads/cats_dogs'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+
+conv_base = VGG16(weights='imagenet', include_top=False,
+    input_shape=(150, 150, 3))
+
+model = models.Sequential()
+
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=2e-5),
+              metrics=['acc'])
+
+
+datagen = ImageDataGenerator(
+    rescale=1./255,
+)
+batch_size = 20
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary'
+    )
+    i = 0
+    for input_batch, labels_batch in generator:
+        features_batch = conv_base.predict(input_batch)
+        features[i * batch_size : (i + 1) * batch_size] = features_batch
+        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break
+
+    return features, labels
+
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+history = model.fit(
+    train_features,
+    train_labels,
+    epochs=30,
+    batch_size=batch_size,
+    validation_data=(validation_features, validation_labels)
+)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
